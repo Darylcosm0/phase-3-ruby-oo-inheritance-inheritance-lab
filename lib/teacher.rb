2e874fb52a233be6f2d65ffc41e868a873cb681b
Darylcosm0 <mutaidaryl@gmail.com>
2023-02-17 09:35:16 +0300

    done

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,10 @@
-class Teacher
+class Student
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
 
-  
-end
+  def raise_hand
+    puts "Pick me!"
+  end  
+end  
